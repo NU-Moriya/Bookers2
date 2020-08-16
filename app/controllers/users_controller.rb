06a8fac3a190874eca1c0e8b85251f15d1b4a24d
nobuko.m <animopara100pre@gmail.com>
2020-08-16 06:07:38 +0000

    created_editprofile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:title, :body )
+    params.require(:user).permit(:name, :introduction )
   end
 end
