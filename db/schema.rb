ecb086767c73595dfd3feaaf8fa8e0741bc30c1f
nobuko.m <animopara100pre@gmail.com>
2020-08-20 00:06:31 +0000

    error_correcting

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_043745) do
+ActiveRecord::Schema.define(version: 2020_08_19_115758) do
 
   create_table "books", force: :cascade do |t|
-    t.text "title"
     t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
