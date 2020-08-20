b47bf96f5a91878217371ee8b5c2255db56ee24e
nobuko.m <animopara100pre@gmail.com>
2020-08-20 01:47:45 +0000

    errors17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  
+  
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
   
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+    flash[:notice] = "you have signed up successfully" 
+  end
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  
   
   protected
 
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,14 +1,7 @@
 class RootController < ApplicationController
   def top
-    @user = User.find(params[:id])
-    redirect_to user_path()
   end
   
   def about
   end 
-  
-  private
-  def user_params
-    params.require(:user).permit(:name, :profile_image, :introduction )
-  end
 end
